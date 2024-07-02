07a0ae52cc84e1188cfbc7de44e3c8fdba249cee
Sichermatias <Sicher2001@gmail.com>
2024-07-02 15:57:14 -0300

    Unificacion de Broker en 1 clase

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,16 @@
 package main;
 
+import dominio.services.broker.BrokerHandler;
+import dominio.services.broker.BrokerReceptorMensajes;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+
 public class Main {
 
     public static void main(String[] args) {
+        BrokerHandler brokerHandler = new BrokerHandler();
+        MqttClient cliente = brokerHandler.conectar();
+        BrokerReceptorMensajes receptor = new BrokerReceptorMensajes();
+        brokerHandler.suscribir(cliente, "dds2024/g12/heladeras/almagro/medrano", receptor);
     }
 }
 
--- src/test/java/ar/edu/utn/frba/dds/services/broker/BrokerTests.java
+++ src/test/java/ar/edu/utn/frba/dds/services/broker/BrokerTests.java
@@ -1,16 +1,10 @@
 package ar.edu.utn.frba.dds.services.broker;
 
-import dominio.services.broker.BrokerConector;
-import dominio.services.broker.BrokerPublicador;
-import dominio.services.broker.BrokerReceptor;
-import dominio.services.broker.BrokerSuscriptor;
-import dominio.services.messageSender.Mensaje;
-import dominio.services.messageSender.adapters.MailSender;
+import dominio.services.broker.*;
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.doNothing;
+import static java.lang.Thread.sleep;
 import static org.mockito.Mockito.mock;
 
 
@@ -18,15 +12,9 @@ public class BrokerTests {
 
     @Test
     void conectarSuscribirPublicarEnBroker(){
-        BrokerConector brokerConector = new BrokerConector();
-        MqttClient cliente = brokerConector.conectar();
-        BrokerSuscriptor brokerSuscriptor = new BrokerSuscriptor();
-        BrokerReceptor brokerReceptor = new BrokerReceptor();
-        brokerSuscriptor.suscribir(cliente, "dds2024/heladeras/almagro/medrano", brokerReceptor);
-
-        BrokerPublicador brokerPublicador = new BrokerPublicador();
-        MqttMessage mensaje = new MqttMessage("Mensaje de prueba".getBytes());
-
-        brokerPublicador.publicar(cliente, "dds2024/heladeras/almagro/medrano", mensaje);
+        BrokerHandler brokerHandler = new BrokerHandler();
+        MqttClient cliente = brokerHandler.conectar();
+        BrokerReceptorMensajes receptor = new BrokerReceptorMensajes();
+        brokerHandler.suscribir(cliente, "dds2024/g12/heladeras/almagro/medrano", receptor);
     }
 }
