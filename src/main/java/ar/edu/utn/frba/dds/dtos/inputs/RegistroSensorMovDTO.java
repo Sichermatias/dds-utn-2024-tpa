6a0f1490423a278e0ef8349dbbd4329682fc18aa
Sichermatias <Sicher2001@gmail.com>
2024-07-02 16:54:36 -0300

    Creacion de DTO desde mqttMessage

--- src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorMovDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorMovDTO.java
@@ -1,5 +1,16 @@
 package ar.edu.utn.frba.dds.dtos.inputs;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
+
+@Getter @Setter
 public class RegistroSensorMovDTO {
     private String idHeladera;
+
+    RegistroSensorMovDTO(MqttMessage mqttMessage){
+        JSONObject json = new JSONObject(mqttMessage.toString());
+        this.idHeladera = json.getString("idHeladera");
+    }
 }
--- src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorTempDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorTempDTO.java
@@ -2,9 +2,17 @@ package ar.edu.utn.frba.dds.dtos.inputs;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
 
 @Getter @Setter
 public class RegistroSensorTempDTO {
     private String idHeladera;
     private Double temperatura;
+
+    RegistroSensorTempDTO(MqttMessage mqttMessage){
+        JSONObject json = new JSONObject(mqttMessage.toString());
+        this.idHeladera = json.getString("idHeladera");
+        this.temperatura = json.getDouble("temperatura");
+    }
 }
