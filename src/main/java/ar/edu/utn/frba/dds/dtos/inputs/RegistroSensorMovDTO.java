7ea776ce3d2d75ba637842e6ab86293ffe03377f
Carrizo-Gonzalo <gocarrizo@frba.utn.edu.ar>
2024-07-02 16:41:51 -0300

    Creo clases dto para sensores

---
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorMovDTO.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.frba.dds.dtos.inputs;
+
+public class RegistroSensorMovDTO {
+    private String idHeladera;
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/RegistroSensorTempDTO.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.dtos.inputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RegistroSensorTempDTO {
+    private String idHeladera;
+    private Double temperatura;
+}
