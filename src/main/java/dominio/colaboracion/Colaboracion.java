d617b91af1f85c9a6e268d2631b90819cbadd93b
Carrizo-Gonzalo <gocarrizo@frba.utn.edu.ar>
2024-08-30 15:14:26 -0300

    Annotations en Colaboracion

--- src/main/java/dominio/colaboracion/Colaboracion.java
+++ src/main/java/dominio/colaboracion/Colaboracion.java
@@ -4,17 +4,32 @@ import dominio.persona.Colaborador;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "colaboracion")
 public class Colaboracion {
+    @Column(name = "nombre", columnDefinition = "VARCHAR(50)")
     private String nombre;
+
+    @Column(name = "tipo", columnDefinition = "VARCHAR(50)")
     @Setter @Getter
     private String tipo;
+
+    @Column(name = "descripcion", columnDefinition = "TEXT")
     private String descripcion;
 
+    @Column(name = "fechaColaboracion", columnDefinition = "DATE")
     public LocalDate fechaColaboracion;
+
+    @OneToOne
+    @JoinColumn(name = "transaccion_id")
     private Transaccion transaccion;
+
+    @ManyToOne
+    @JoinColumn(name = "colaborador_id")
     @Setter @Getter
     private Colaborador colaborador;
 
