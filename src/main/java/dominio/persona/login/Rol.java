0e3b73a51363dc43b74d944aaea12577fbc3e7b7
tinchoi99 <martinibarra99@gmail.com>
2024-09-11 17:29:19 -0300

    Persistencia Usuarios, roles y Permisos

--- src/main/java/dominio/persona/login/Rol.java
+++ src/main/java/dominio/persona/login/Rol.java
@@ -3,13 +3,26 @@ package dominio.persona.login;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "rol")
 @Getter
 public class Rol {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "rolNombre", columnDefinition = "VARCHAR(50)")
     @Setter
     private String nombreRol;
+
+    @ManyToMany
+    @JoinTable(name = "rol_permiso",
+                joinColumns = @JoinColumn(name = "rol_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "permiso_id", referencedColumnName = "id"))
     private List<Permiso> permisos;
 
     public Rol() {
