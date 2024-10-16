f15e24d08b019a0f72ca427c624a647e08c9561c
Bietti-Federico <fbietti@frba.utn.edu.ar>
2024-10-16 00:40:58 -0300

    Login rebota por credenciales
    Login rebota por credenciales incorrectas

--- src/main/java/ar/edu/utn/frba/dds/controllers/LoginController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/LoginController.java
@@ -5,10 +5,7 @@ import ar.edu.utn.frba.dds.models.repositories.imp.UsuarioRepositorio;
 import io.javalin.http.Context;
 import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class LoginController implements ICrudViewsHandler{
 
@@ -20,17 +17,9 @@ public class LoginController implements ICrudViewsHandler{
 
     @Override
     public void index(Context context) {
-        Map<String, Object> model = new HashMap<>();
-        if(context.sessionAttribute("usuario_id")!= null){
-            model.put("logeado",true);
-           //model.put("administrador",esAdmin(context));
-            context.render("Landing-Page.hbs", model);
-        }else{
-            model.put("logeado",false);
-
-            context.render("Login.hbs", model);
-        }
-
+        String error = context.sessionAttribute("login_error");
+        context.sessionAttribute("login_error", null);
+        context.render("Login.hbs", Collections.singletonMap("error", error));
     }
 
     @Override
@@ -59,22 +48,24 @@ public class LoginController implements ICrudViewsHandler{
         if (!usuarios.isEmpty()) {
             Usuario usuario = usuarios.get(0);
             if (usuario.getContrasenia().equals(context.formParam("password"))) {
-                // Almacenar el ID del usuario y el rol en la sesion
                 context.sessionAttribute("usuario_id", usuario.getId());
                 context.sessionAttribute("tipo_rol", usuario.getRol().getTipo().name());
                 context.redirect("/");
             } else {
                 // Manejar el caso donde la contraseña es incorrecta
-                context.redirect("/login?error=contraseña_incorrecta");
+                context.sessionAttribute("login_error", "Credenciales incorrectas");
+                context.redirect("/login");
             }
         } else {
             // Manejar el caso donde no se encuentra el usuario
-            context.redirect("/login?error=usuario_no_encontrado");
+            context.sessionAttribute("login_error", "Credenciales incorrectas");
+            context.redirect("/login");
         }
     }
+    public void error(Context context) {
 
-
-    @Override
+    }
+        @Override
     public void delete(Context context) {
 
     }
