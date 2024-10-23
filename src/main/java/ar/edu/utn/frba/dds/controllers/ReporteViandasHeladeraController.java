824eef5de9895fedf08530a173e030ad5b9dbf6a
tinchoi99 <martinibarra99@gmail.com>
2024-10-22 22:32:26 -0300

    correccion fallos_heladera.hbs

---
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ReporteViandasHeladeraController.java
@@ -0,0 +1,43 @@
+package ar.edu.utn.frba.dds.controllers;
+
+import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+import io.javalin.http.Context;
+
+public class ReporteViandasHeladeraController implements ICrudViewsHandler, WithSimplePersistenceUnit {
+    public ReporteViandasHeladeraController() {
+    }
+
+    @Override
+    public void create(Context context) {
+
+    }
+    @Override
+    public void index(Context context){
+
+    }
+    @Override
+    public void show(Context context){
+
+    }
+    @Override
+    public void save(Context context) {
+
+    }
+
+    @Override
+    public void edit(Context context) {
+
+    }
+
+    @Override
+    public void update(Context context) {
+
+    }
+
+    @Override
+    public void delete(Context context) {
+
+    }
+
+}
