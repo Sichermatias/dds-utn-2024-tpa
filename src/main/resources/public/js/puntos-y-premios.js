4932ca9b0576b527820f2a22a7252ba7fd40aaef
Carrizo-Gonzalo <gocarrizo@frba.utn.edu.ar>
2024-11-24 17:55:08 -0300

    Canjear premio pide confirmacion

--- src/main/resources/public/js/puntos-y-premios.js
+++ src/main/resources/public/js/puntos-y-premios.js
@@ -12,3 +12,19 @@ function filterProducts() {
         }
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const confirmModal = document.getElementById('canjearPremioModal');
+    const puntajeDestacado = confirmModal.querySelector('#puntaje-destacado');
+
+    confirmModal.addEventListener('show.bs.modal', (event) => {
+        // Botón que activó la modal
+        const button = event.relatedTarget;
+
+        // Obtén los puntos desde el data attribute
+        const puntos = button.getAttribute('data-puntos');
+
+        // Actualiza el contenido del marcador con estilos
+        puntajeDestacado.textContent = puntos;
+    });
+});
